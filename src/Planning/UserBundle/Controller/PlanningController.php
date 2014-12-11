15bdf135b45ca755802e21d1c383781b7a3319a3
didjo972 <didjo972@gmail.com>
2014-12-11 22:55:53 +0100

    - Affichage du planning par utilisateur

--- src/Planning/UserBundle/Controller/PlanningController.php
+++ src/Planning/UserBundle/Controller/PlanningController.php
@@ -17,7 +17,8 @@ namespace Planning\UserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class PlanningController extends Controller {
-    public function planningAction() {
+    public function planningAction() {        
+    
         $Promotion = $this->getDoctrine()
                 ->getRepository('PlanningUserBundle:Promotion')
                 ->findOneBy(array('numPromotion'=>'1'));
@@ -27,13 +28,30 @@ class PlanningController extends Controller {
         return $this->render('PlanningUserBundle:Advert:planning.html.twig', array('Cours' => $Cours));
     }
     public function annuelAction(){
+        // Pour charger un utilisateur
+        $connexion = $this->get('security.context')->getToken()->getUser();
+        echo $connexion;
+        //TODO Résoudre le problème de récupération du User
+//        $User = $this->getDoctrine()
+//                ->getRepository('PlanningUserBundle:User')
+//                ->findOneBy(array('username'=>$connexion));
+//        echo $User->getId();
+//        $Eleve = $this->getDoctrine()
+//                ->getRepository('PlanningUserBundle:Eleve')
+//                ->findOneBy(array('useriduser'=>$User));
+//        echo $Eleve;
+//        $Promotion = $this->getDoctrine()
+//                ->getRepository('PlanningUserBundle:Promotion')
+//                ->findOneBy(array('numPromotion'=>$Eleve->promotionidpromotion()));
+//        echo $Promotion;
+//        $Cours = $this->getDoctrine()
+//                ->getRepository('PlanningUserBundle:Cours')
+//                ->findBy(array('promotionpromotion'=>$Promotion->getNumPromotion()));
         
-        $Promotion = $this->getDoctrine()
-                ->getRepository('PlanningUserBundle:Promotion')
-                ->findOneBy(array('numPromotion'=>'1',));
         $Cours = $this->getDoctrine()
                 ->getRepository('PlanningUserBundle:Cours')
-                ->findBy(array('promotionpromotion'=>$Promotion->getIdpromotion()));
+                ->findBy(array('promotionidpromotion'=>$Promotion->getIdpromotion()));
+        $Promotion = 1;
 
         return $this->render('PlanningUserBundle:Advert:planning_annuel.html.twig', array('Cours' => $Cours,
                                                                                           'Promotion' => $Promotion));
